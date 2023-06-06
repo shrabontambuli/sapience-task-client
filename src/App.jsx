74f9f74c6e94735621268641bf67af01950095e7
ShrabonTambuli <shrabonsa@gmail.com>
2023-06-06 23:14:47 +0600

    create footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { Outlet } from "react-router-dom"
 import Navbar from "./Pages/Shared/Navbar/Navbar"
+import Footer from "./Pages/Shared/Footer/Footer"
 
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Outlet/>
+      <Footer/>
     </>
   )
 }
---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+
+
+
+const Footer = () => {
+    return (
+        <div className="mt-10 bg-[#a7a4a4] text-black py-10">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-5 text-center font-serif items-center">
+                <div className="inline-flex justify-center items-center">
+                    <img className="w-16" src="/image/pngwing.com (1).png" alt="" />
+                    <h1 className="text-3xl font-semibold ms-2">Express Music <br /> Academy</h1>
+                </div>
+                <div>
+                    <h1 className="text-2xl font-semibold mb-3">Services</h1>
+                    <p>Guitar</p>
+                    <p>Piano</p>
+                    <p>Drums</p>
+                    <p>Violin</p>
+                </div>
+                <div>
+                    <h1 className="text-2xl font-semibold mb-3">Contact Us</h1>
+                    <p>+880 3525444545</p>
+                    <p>Email: shrabon@gmail.com </p>
+                    <p>ADD: Pathrail,Delduar,</p>
+                    <p>Tangail</p>
+                </div>
+            </div>
+            <hr className="mt-8 mx-20 border-black border-2" />
+            <div className="text-center mt-10">
+                <p>Copyright © 2023 - All right reserved</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
