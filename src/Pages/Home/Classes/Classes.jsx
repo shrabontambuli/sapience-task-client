ae04dbe592ad3ccf2880b0962c43e0cdb2aa5b78
ShrabonTambuli <shrabonsa@gmail.com>
2023-06-08 00:31:12 +0600

    added framer motion in class section

--- src/Pages/Home/Classes/Classes.jsx
+++ src/Pages/Home/Classes/Classes.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 
 const Classes = () => {
@@ -13,23 +14,25 @@ const Classes = () => {
     return (
         <div className="mt-20 container mx-auto">
             <div>
-                <h1 className="text-4xl font-serif font-semibold text-center">Our Musical Instruments <br /> Class</h1>
+                <h1 className="text-3xl font-serif font-semibold text-center">Our Popular Musical Instruments <br /> Class</h1>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-14">
                 {
                     data.map(d =>
-                     <div key={d._id} className="card w-80 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src={d.picture} alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{d.class_name}</h2>
-                            <p>{d.details}</p>
-                            <div className="card-actions">
-                                <button className="btn btn-outline btn-warning">Learn More</button>
+                        <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} key={d._id}>
+                            <div className="card w-80 bg-base-100 shadow-xl">
+                                <figure className="px-10 pt-10">
+                                    <img src={d.picture} alt="Shoes" className="rounded-xl" />
+                                </figure>
+                                <div className="card-body items-center text-center">
+                                    <h2 className="card-title">{d.class_name}</h2>
+                                    <p>{d.details}</p>
+                                    <div className="card-actions">
+                                        <button className="btn btn-outline btn-warning">Learn More</button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        </motion.div>
                     )
                 }
             </div>
