1242dbf06d0ddfa3560ddbe0f4913df9c6a15edc
ShrabonTambuli <shrabonsa@gmail.com>
2023-06-07 14:29:16 +0600

    handle form submit

--- src/Pages/LogIn/Login.jsx
+++ src/Pages/LogIn/Login.jsx
@@ -1,7 +1,12 @@
+import { useForm } from "react-hook-form";
 import { Form, Link } from "react-router-dom";
 
 
 const Login = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const onSubmit = data =>{
+        console.log(data);
+    }
     return (
         <div className="mt-10">
             <div className="hero min-h-screen bg-base-200">
@@ -11,19 +16,19 @@ const Login = () => {
                         <img className="h-96 w-full rounded-3xl mt-3" src="/image/login.jpg" alt="" />
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <Form className="card-body">
+                        <Form onSubmit={handleSubmit(onSubmit)} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email"
+                                <input type="text" {...register("email", { required: true })} placeholder="email"
                                     name="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password"
+                                <input type="password" {...register("password", { required: true })} placeholder="password"
                                     name="password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,7 +1,12 @@
+import { useForm } from "react-hook-form";
 import { Form, Link } from "react-router-dom";
 
 
 const SignUp = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }
     return (
         <div className="mt-10">
             <div className="hero min-h-screen bg-base-200">
@@ -11,40 +16,55 @@ const SignUp = () => {
                         <img className="h-96 w-full rounded-3xl mt-3" src="../../../public/image/register.webp" alt="" />
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <Form className="card-body">
+                        <Form onSubmit={handleSubmit(onSubmit)} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" placeholder="name"
+                                <input type="text" {...register("name", { required: true })} placeholder="name"
                                     name="name" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email"
+                                <input type="text" {...register("email", { required: true })} placeholder="email"
                                     name="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password"
+                                <input type="password" {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/
+                                })}
+                                    placeholder="password"
                                     name="password" className="input input-bordered" />
+                                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase and one special character </p>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Confirm Password</span>
                                 </label>
-                                <input type="password" placeholder="confirm password"
+                                <input type="password" {...register("confirmPassword", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/
+                                })}
+                                 placeholder="confirm password"
                                     name="confirmPassword" className="input input-bordered" />
+                                
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Photo Url</span>
                                 </label>
-                                <input type="text" placeholder="photo"
+                                <input type="text" {...register("photo", { required: true })} placeholder="photo"
                                     name="photo" className="input input-bordered" />
                             </div>
                             <div className="form-control mt-6">
