f8470102eca475f8c591a95596a3f4dd3cb19594
Shrabon Tambuli <shrabonsa@gmail.com>
2025-04-01 19:03:18 +0600

    change code

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,6 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-const queryClient = new QueryClient()
 import ErrorPage from './Pages/Shared/ErrorPage/ErrorPage.jsx';
 import Login from './Pages/LogIn/Login.jsx';
 import SignUp from './Pages/SignUp/SignUp.jsx';
@@ -67,11 +62,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <QueryClientProvider client={queryClient}>
         <RouterProvider router={router}>
           <App />
         </RouterProvider>
-      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
